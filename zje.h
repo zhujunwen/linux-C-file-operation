486fdb3ba25bfe9f999ebbbbc247bb596193d1d3
ZJE <349108503@qq.com>
2017-04-08 20:17:10 +0800

    Create zje.h

---
+++ zje.h
@@ -0,0 +1,37 @@
+#ifndef __ZJE__
+#define __ZJE__
+
+/*head files*/
+#include <stdio.h>  
+//#include <mysql.h>   //所需头文件  
+#include <sys/types.h>  
+#include <sys/stat.h>  
+#include <fcntl.h>   
+#include <stdlib.h>  
+#include <string.h>  
+#include <strings.h>  
+#include <unistd.h>  
+#include <errno.h>  
+#include <sys/stat.h>  
+#include <dirent.h>  
+#include <sys/mman.h>  
+#include <sys/wait.h>  
+#include <signal.h>  
+#include <sys/ipc.h>  
+#include <sys/shm.h>  
+#include <sys/msg.h>  
+#include <sys/sem.h>  
+#include <pthread.h>  
+#include <semaphore.h>  
+#include <poll.h>  
+#include <sys/epoll.h>  
+#include <sys/socket.h>  
+#include <netinet/in.h>  
+#include <arpa/inet.h>  
+#include <netinet/in.h>  
+
+struct sockaddr_in saddr;  
+struct sockaddr_in caddr; 
+int size; 
+
+#endif
